5f903c6a700243ea5b3e588bb73be706b5f8f19a
kateryna <kateryna_neholiuk@ukr.net>
2021-01-02 15:30:38 +0200

    data img for front page menu

---
+++ data/drinks-data.js
@@ -0,0 +1,206 @@
+const drinksData = {
+    selector: '#drinks',
+    items: [
+    {
+        photo: 'coca-cola-1.jpg',
+        title: 'Coca-Cola',
+        description: '1 l',
+        price: '2,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'coca-cola-zero.jpg',
+        title: 'Coca-Cola Zero',
+        description: '0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'coca-cola-0.5.jpg',
+        title: 'Coca-Cola',
+        description: '0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'fanta.jpg',
+        title: 'Fanta',
+        description: '0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'sprite.jpg',
+        title: 'Sprite',
+        description: '0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'negazuotas-vanduo.jpg',
+        title: 'Negazuotas vanduo',
+        description: '0,5 l',
+        price: '1,30 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'gazuotas-vanduo.jpg',
+        title: 'Gazuotas vanduo',
+        description: '',
+        price: '1,30 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'pienas.jpg',
+        title: 'Pienas MARGĖ, 3,2%, UAT',
+        description: '3,2% riebumo pienas, 1 L Apdorotas ultraaukšta temperatūra (UAT), 1 l',
+        price: '1,20 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-ivairiu-vaisiu-1.jpg',
+        title: 'Įvairių vaisių gėrimas CIDO',
+        description: '1 l',
+        price: '2,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-apelsinu-1.jpg',
+        title: 'Apelsinų sultys CIDO 100%',
+        description: '1 l',
+        price: '2,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-obuoliu-1.jpg',
+        title: 'Obuolių sultys CIDO 100%',
+        description: '1 l',
+        price: '2,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-pomidoru-1.jpg',
+        title: 'Pomidorų sultys CIDO 100%',
+        description: '1 l',
+        price: '2,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-apelsinu-0.3.jpg',
+        title: 'Apelsinų sultys CIDO (100%)',
+        description: '0,3 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-obuoliu-0.3.jpg',
+        title: 'Obuolių sultys CIDO (100%)',
+        description: '0,3 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-pomidoru-0.3.jpg',
+        title: 'Pomidorų sultys CIDO (100%)',
+        description: '0,3 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'frutto-ivairiu-vaisiu.png',
+        title: 'Įvairių vaisių gėrimas FRUTTO',
+        description: '0,5 l',
+        price: '1,30 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'vasaros-uogu-frutto.png',
+        title: 'Vasaros uogų sulčių gėrimas FRUTTO',
+        description: '0,5 l',
+        price: '1,30 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'cido-ivairiu-vaisiu.png',
+        title: 'Įvairių vaisių gėrimas CIDO',
+        description: '0,2 l',
+        price: '0,60 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'fuzetea-lemon.jpg',
+        title: 'Fuzetea Lemon',
+        description: '0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'fuzetea-peach.jpg',
+        title: 'Fuzetea Peach',
+        description: '0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'fuzetea-green-citrus.jpg',
+        title: 'Fuzetea Green Citrus',
+        description: 'Šalta žalia arbata su citrina., 0,5 l',
+        price: '1,50 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'nealkoholinis-baltas-alus.jpg',
+        title: 'Nealkoholinis kvietinis BALTAS alus',
+        description: 'Nealkoholinis kvietinis BALTAS alus, 0,5 l',
+        price: '2,00 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'nealkoholinis-alus-pilsner.jpg',
+        title: 'Nealkoholinis šviesusis alus PILSNER',
+        description: 'Nealkoholinis šviesusis alus PILSNER, 0,5 l',
+        price: '2,00 €',
+        button: 'Į krepšelį'
+    },
+    
+    {
+        photo: 'nealkoholinis-radler.jpg',
+        title: 'Nealkoholinis kokteilis RADLER',
+        description: '0,5 l',
+        price: '2,00 €',
+        button: 'Į krepšelį'
+    },
+
+    {
+        photo: 'red-bull.jpg',
+        title: 'Energinis gėrimas RED BULL',
+        description: '0,25 l',
+        price: '2,50 €',
+        button: 'Į krepšelį'
+    }
+]
+}
+
+export { drinksData}
